fbfbf38aa8fe896eeeeb9bc7e60b57e81556ac03
RyanNishioka <103604460+RyanNishioka@users.noreply.github.com>
2022-04-14 00:12:05 -0300

    Add files via upload

---
+++ bootstrap-3.3.7-dist/js/navbar-animation-fix.js
@@ -0,0 +1,7 @@
+$('#bs-example-navbar-collapse-1').on('show.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform', 'translate(-50%,10%');
+});
+
+$('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform', 'translate(-50%,-50%');
+});
